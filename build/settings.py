4c07a1d7f4472b96b2e3e6c313b69aa9ada5cec6
Tim Horton <hortont424@gmail.com>
2009-11-18 00:22:04 -0500

    Move over to real url

--- build/settings.py
+++ build/settings.py
@@ -1,7 +1,7 @@
 import os
 
 page_size = 10
-www_prefix = "http://hortont.com/blog2"
+www_prefix = "http://hortont.com/blog"
 
 if os.uname()[1] == "Kaylee.local":
     www_prefix = "http://localhost/~hortont/output"
