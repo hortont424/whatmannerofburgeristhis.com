bbb0dd6fba5723a2c3e2ab6277288482bf35c4ed
Matt Arsenault <arsenm2@rpi.edu>
2010-05-07 23:16:24 -0400

    Blog post about cornify ROFLCon picture

--- build/settings.py
+++ build/settings.py
@@ -7,9 +7,9 @@ static_prefix = "http://files.whatmannerofburgeristhis.com/www/"
 
 # Set this to the path that you keep your local copy in, and where it's served
 # to (this should point to the output directory)
-if os.getcwd() == "/Users/hortont/src/whatmannerofburgeristhis.com":
-    www_prefix = "file:///Users/hortont/src/whatmannerofburgeristhis.com/output/"
-    static_prefix = "file:///Users/hortont/src/whatmannerofburgeristhis.com/output/"
+if os.getcwd() == "/Volumes/Users/matt/src/whatmannerofburgeristhis.com":
+    www_prefix = "file:///Volumes/Users/matt/src/whatmannerofburgeristhis.com/output/"
+    static_prefix = "file:///Volumes/Users/matt/src/whatmannerofburgeristhis.com/output/"
 
 blog_dir = "blog/"
 
@@ -21,12 +21,12 @@ def w(u):
 # Set up a hierarchy for categories here. I think this is mostly just for URLs
 # but I really don't remember.
 def categoryURLFromName(n):
-    if n == "gnome":
-        return "code/gnome"
-    if n == "fooding":
-        return "personal/fooding"
-    if n == "video":
-        return "school/video"
+#     if n == "gnome":
+#         return "code/gnome"
+#     if n == "fooding":
+#         return "personal/fooding"
+#     if n == "video":
+#         return "school/video"
     return n
 
 def categoryDisplayName(n):
